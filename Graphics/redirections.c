faf8d12e7633052d69a12598cf02c3ab99c8a8b2
Augustin Leconte <augustin.leconte@epitech.eu>
2017-02-21 14:13:43 +0100

    repo

--- Graphics/redirections.c
+++ Graphics/redirections.c
@@ -5,7 +5,7 @@
 ** Login   <augustin.leconte@epitech.eu>
 **
 ** Started on  Mon Feb 20 19:45:33 2017 augustin leconte
-** Last update Tue Feb 21 10:16:47 2017 augustin leconte
+** Last update Tue Feb 21 11:50:44 2017 augustin leconte
 */
 
 #include <sys/stat.h>
@@ -29,11 +29,7 @@ void print_ufo()
 {
   int len;
 
-  start_color();
-  init_pair(1, COLOR_BLUE, COLOR_BLACK);
-  init_pair(2, COLOR_WHITE, COLOR_BLACK);
   len = my_strlen("/                                                             \\") / 2;
-  attron(COLOR_PAIR(1));
   mvprintw(1, COLS / 2 - len, "                         ______________");
   mvprintw(2, COLS / 2 - len, "                        /              \\");
   mvprintw(3, COLS / 2 - len, "             __________/________________\\__________");
@@ -44,40 +40,45 @@ void print_ufo()
   mvprintw(8, COLS / 2 - len, "\\_____________________________________________________________/");
   mvprintw(9, COLS / 2 - len, "   \\                                                      /");
   mvprintw(10, COLS / 2 - len, "    \\____________________________________________________/");
-  attroff(COLOR_PAIR(1));
 }
 
-void fill_tab(char tab[30][30])
+void fill_tab(char tab[30][40])
 {
   int i;
   int j;
 
   i = -1;
-  while (++i < 30)
+  while (++i < 29)
   {
     j = 0;
     tab[i][0] = '|';
-    tab[i][29] = '|';
-    while (++j < 29)
-      tab[i][j] = ' ';
+    tab[i][39] = '|';
+    while (++j < 39)
+      tab[i][j] = '-';
   }
   j = 0;
-  while (++j < 29)
-    tab[30][j] = '_';
+  while (++j < 39)
+    tab[29][j] = '_';
 }
 
-void print_tab(char tab[30][30])
+void print_tab(char tab[30][40])
 {
   int i;
+  int j;
 
   i = -1;
-  while (++i < 30)
-    mvprintw((LINES / 2) + i, (COLS / 2) - 15, "%s\n", tab[i]);
+  while (++i < 29)
+  {
+    j = -1;
+    while (++j < 39)
+      mvprintw((LINES / 2) + i, (COLS / 2) - 20 + j, "%c", tab[i][j]);
+  }
+  printw("\n");
 }
 
 void playing()
 {
-  char tab[30][30];
+  char tab[30][40];
 
   fill_tab(tab);
   clear();
