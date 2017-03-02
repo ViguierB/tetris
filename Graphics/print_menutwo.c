a495f607d8fc16b6646096d2504e2b9447336523
Augustin Leconte <augustin.leconte@epitech.eu>
2017-03-02 13:11:16 +0100

    arrivée de la couleur omg

--- Graphics/print_menutwo.c
+++ Graphics/print_menutwo.c
@@ -5,7 +5,7 @@
 ** Login   <augustin.leconte@epitech.eu>
 **
 ** Started on  Thu Mar  2 12:08:04 2017 augustin leconte
-** Last update Thu Mar  2 12:11:39 2017 augustin leconte
+** Last update Thu Mar  2 13:10:34 2017 augustin leconte
 */
 
 #include <sys/stat.h>
@@ -33,23 +33,31 @@ void print_line(int where)
 
 void end_name(int len)
 {
-  mvprintw(3, COLS / 2 - len + 42, "**");
-  mvprintw(3, COLS / 2 - len + 45, "******");
-  mvprintw(4, COLS / 2 - len + 4, "**");
-  mvprintw(4, COLS / 2 - len + 11, "**");
+  mvprintw(2, COLS / 2 - len + 24, "**");
+  mvprintw(3, COLS / 2 - len + 24, "**");
   mvprintw(4, COLS / 2 - len + 24, "**");
+  mvprintw(5, COLS / 2 - len + 24, "**");
+  attron(COLOR_PAIR(9));
+  mvprintw(1, COLS / 2 - len + 31, "******");
+  mvprintw(2, COLS / 2 - len + 31, "**");
+  mvprintw(2, COLS / 2 - len + 37, "**");
+  mvprintw(3, COLS / 2 - len + 31, "******");
   mvprintw(4, COLS / 2 - len + 31, "**");
   mvprintw(4, COLS / 2 - len + 37, "**");
-  mvprintw(4, COLS / 2 - len + 42, "**");
-  mvprintw(4, COLS / 2 - len + 49, "**");
-  mvprintw(5, COLS / 2 - len + 4, "**");
-  mvprintw(5, COLS / 2 - len + 11, "********");
-  mvprintw(5, COLS / 2 - len + 24, "**");
   mvprintw(5, COLS / 2 - len + 31, "**");
   mvprintw(5, COLS / 2 - len + 37, "**");
+  attron(COLOR_PAIR(6));
+  mvprintw(1, COLS / 2 - len + 42, "**");
+  mvprintw(2, COLS / 2 - len + 42, "**");
+  mvprintw(3, COLS / 2 - len + 42, "**");
+  mvprintw(4, COLS / 2 - len + 42, "**");
   mvprintw(5, COLS / 2 - len + 42, "**");
+  attron(COLOR_PAIR(8));
+  mvprintw(1, COLS / 2 - len + 45, "******");
+  mvprintw(2, COLS / 2 - len + 45, "**");
+  mvprintw(3, COLS / 2 - len + 45, "******");
+  mvprintw(4, COLS / 2 - len + 49, "**");
   mvprintw(5, COLS / 2 - len + 45, "******");
-  attroff(COLOR_PAIR(3));
 }
 
 void print_name()
@@ -57,26 +65,27 @@ void print_name()
   int len;
 
   start_color();
+  init_pair(5, COLOR_YELLOW, COLOR_YELLOW);
+  init_pair(6, COLOR_GREEN, COLOR_GREEN);
+  init_pair(7, COLOR_RED, COLOR_RED);
+  init_pair(9, COLOR_MAGENTA, COLOR_MAGENTA);
+  init_pair(8, COLOR_CYAN, COLOR_CYAN);
   init_pair(3, COLOR_BLUE, COLOR_BLUE);
-  attron(COLOR_PAIR(3));
+  attron(COLOR_PAIR(5));
   len = 53 / 2;
   mvprintw(1, COLS / 2 - len, "**********");
-  mvprintw(1, COLS / 2 - len + 11, "********");
-  mvprintw(1, COLS / 2 - len + 20, "**********");
-  mvprintw(1, COLS / 2 - len + 31, "******");
-  mvprintw(1, COLS / 2 - len + 42, "**");
-  mvprintw(1, COLS / 2 - len + 45, "******");
   mvprintw(2, COLS / 2 - len + 4, "**");
-  mvprintw(2, COLS / 2 - len + 11, "**");
-  mvprintw(2, COLS / 2 - len + 24, "**");
-  mvprintw(2, COLS / 2 - len + 31, "**");
-  mvprintw(2, COLS / 2 - len + 37, "**");
-  mvprintw(2, COLS / 2 - len + 42, "**");
-  mvprintw(2, COLS / 2 - len + 45, "**");
   mvprintw(3, COLS / 2 - len + 4, "**");
+  mvprintw(4, COLS / 2 - len + 4, "**");
+  mvprintw(5, COLS / 2 - len + 4, "**");
+  attron(COLOR_PAIR(7));
+  mvprintw(1, COLS / 2 - len + 11, "********");
+  mvprintw(2, COLS / 2 - len + 11, "**");
   mvprintw(3, COLS / 2 - len + 11, "******");
-  mvprintw(3, COLS / 2 - len + 24, "**");
-  mvprintw(3, COLS / 2 - len + 31, "******");
+  mvprintw(4, COLS / 2 - len + 11, "**");
+  mvprintw(5, COLS / 2 - len + 11, "********");
+  attron(COLOR_PAIR(3));
+  mvprintw(1, COLS / 2 - len + 20, "**********");
   end_name(len);
 }
 
