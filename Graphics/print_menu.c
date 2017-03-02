8fc3f5682eb4e9182bbed61a5c1a5b9858a07d02
Augustin Leconte <augustin.leconte@epitech.eu>
2017-03-02 12:14:19 +0100

    La norme c'est bon reste plus qu'a faire verifié par le patron

--- Graphics/print_menu.c
+++ Graphics/print_menu.c
@@ -5,7 +5,7 @@
 ** Login   <augustin.leconte@epitech.eu>
 **
 ** Started on  Tue Feb 21 16:03:30 2017 augustin leconte
-** Last update Thu Mar  2 12:07:03 2017 augustin leconte
+** Last update Thu Mar  2 12:13:53 2017 augustin leconte
 */
 
 #include <sys/stat.h>
@@ -18,100 +18,19 @@
 
 int my_strlen();
 
-void print_line(int where)
+void end_quit(int len)
 {
-  int i;
-
-  i = -1;
-  start_color();
-  init_pair(4, COLOR_WHITE, COLOR_WHITE);
-  attron(COLOR_PAIR(4));
-  while (++i < COLS)
-    mvprintw(where, i, "-");
-  attroff(COLOR_PAIR(4));
-}
-
-void print_name()
-{
-  int len;
-
-  start_color();
-  init_pair(3, COLOR_BLUE, COLOR_BLUE);
-  attron(COLOR_PAIR(3));
-  len = 53 / 2;
-  mvprintw(1, COLS / 2 - len, "**********");
-  mvprintw(1, COLS / 2 - len + 11, "********");
-  mvprintw(1, COLS / 2 - len + 20, "**********");
-  mvprintw(1, COLS / 2 - len + 31, "******");
-  mvprintw(1, COLS / 2 - len + 42, "**");
-  mvprintw(1, COLS / 2 - len + 45, "******");
-  mvprintw(2, COLS / 2 - len + 4, "**");
-  mvprintw(2, COLS / 2 - len + 11, "**");
-  mvprintw(2, COLS / 2 - len + 24, "**");
-  mvprintw(2, COLS / 2 - len + 31, "**");
-  mvprintw(2, COLS / 2 - len + 37, "**");
-  mvprintw(2, COLS / 2 - len + 42, "**");
-  mvprintw(2, COLS / 2 - len + 45, "**");
-  mvprintw(3, COLS / 2 - len + 4, "**");
-  mvprintw(3, COLS / 2 - len + 11, "******");
-  mvprintw(3, COLS / 2 - len + 24, "**");
-  mvprintw(3, COLS / 2 - len + 31, "******");
-  mvprintw(3, COLS / 2 - len + 42, "**");
-  mvprintw(3, COLS / 2 - len + 45, "******");
-  mvprintw(4, COLS / 2 - len + 4, "**");
-  mvprintw(4, COLS / 2 - len + 11, "**");
-  mvprintw(4, COLS / 2 - len + 24, "**");
-  mvprintw(4, COLS / 2 - len + 31, "**");
-  mvprintw(4, COLS / 2 - len + 37, "**");
-  mvprintw(4, COLS / 2 - len + 42, "**");
-  mvprintw(4, COLS / 2 - len + 49, "**");
-  mvprintw(5, COLS / 2 - len + 4, "**");
-  mvprintw(5, COLS / 2 - len + 11, "********");
-  mvprintw(5, COLS / 2 - len + 24, "**");
-  mvprintw(5, COLS / 2 - len + 31, "**");
-  mvprintw(5, COLS / 2 - len + 37, "**");
-  mvprintw(5, COLS / 2 - len + 42, "**");
-  mvprintw(5, COLS / 2 - len + 45, "******");
+  mvprintw(33, COLS / 2 - len + 19, "**");
+  mvprintw(33, COLS / 2 - len + 22, "**");
+  mvprintw(33, COLS / 2 - len + 29, "**");
+  mvprintw(34, COLS / 2 - len + 8, "**");
+  mvprintw(34, COLS / 2 - len + 13, "******");
+  mvprintw(34, COLS / 2 - len + 22, "**");
+  mvprintw(34, COLS / 2 - len + 29, "**");
   attroff(COLOR_PAIR(3));
-}
-
-void play()
-{
-  int len;
-
-  attron(COLOR_PAIR(3));
-  len = 35 / 2;
-  mvprintw(20, COLS / 2 - len, "********");
-  mvprintw(20, COLS / 2 - len + 9, "**");
-  mvprintw(20, COLS / 2 - len + 18, "****");
-  mvprintw(20, COLS / 2 - len + 25, "**");
-  mvprintw(20, COLS / 2 - len + 33, "**");
-  mvprintw(21, COLS / 2 - len, "**");
-  mvprintw(21, COLS / 2 - len + 6, "**");
-  mvprintw(21, COLS / 2 - len + 9, "**");
-  mvprintw(21, COLS / 2 - len + 16, "**");
-  mvprintw(21, COLS / 2 - len + 22, "**");
-  mvprintw(21, COLS / 2 - len + 25, "**");
-  mvprintw(21, COLS / 2 - len + 33, "**");
-  mvprintw(22, COLS / 2 - len, "********");
-  mvprintw(22, COLS / 2 - len + 9, "**");
-  mvprintw(22, COLS / 2 - len + 16, "********");
-  mvprintw(22, COLS / 2 - len + 27, "**");
-  mvprintw(22, COLS / 2 - len + 31, "**");
-  mvprintw(23, COLS / 2 - len, "**");
-  mvprintw(23, COLS / 2 - len + 9, "**");
-  mvprintw(23, COLS / 2 - len + 16, "**");
-  mvprintw(23, COLS / 2 - len + 22, "**");
-  mvprintw(23, COLS / 2 - len + 29, "**");
-  mvprintw(24, COLS / 2 - len, "**");
-  mvprintw(24, COLS / 2 - len + 9, "******");
-  mvprintw(24, COLS / 2 - len + 16, "**");
-  mvprintw(24, COLS / 2 - len + 22, "**");
-  mvprintw(24, COLS / 2 - len + 29, "**");
-  attroff(COLOR_PAIR(3));
-  // attron(COLOR_PAIR(4) | A_UNDERLINE);
-  // mvprintw(25, COLS / 2 - len, "                              ");
-  // attroff(COLOR_PAIR(4) | A_UNDERLINE);
+  attron(COLOR_PAIR(4) | A_UNDERLINE);
+  mvprintw(35, COLS / 2 - len, "***********************************");
+  attroff(COLOR_PAIR(4) | A_UNDERLINE);
 }
 
 void quit()
@@ -139,17 +58,21 @@ void quit()
   mvprintw(32, COLS / 2 - len + 29, "**");
   mvprintw(33, COLS / 2 - len + 2, "******");
   mvprintw(33, COLS / 2 - len + 11, "**");
-  mvprintw(33, COLS / 2 - len + 19, "**");
-  mvprintw(33, COLS / 2 - len + 22, "**");
-  mvprintw(33, COLS / 2 - len + 29, "**");
-  mvprintw(34, COLS / 2 - len + 8, "**");
-  mvprintw(34, COLS / 2 - len + 13, "******");
-  mvprintw(34, COLS / 2 - len + 22, "**");
-  mvprintw(34, COLS / 2 - len + 29, "**");
+end_quit(len);
+}
+
+void end_help(int len)
+{
+  mvprintw(43, COLS / 2 - len + 25, "**");
+  mvprintw(44, COLS / 2 - len, "**");
+  mvprintw(44, COLS / 2 - len + 6, "**");
+  mvprintw(44, COLS / 2 - len + 9, "********");
+  mvprintw(44, COLS / 2 - len + 18, "********");
+  mvprintw(44, COLS / 2 - len + 25, "**");
   attroff(COLOR_PAIR(3));
-  // attron(COLOR_PAIR(4) | A_UNDERLINE);
-  // mvprintw(35, COLS / 2 - len, "                              ");
-  // attroff(COLOR_PAIR(4) | A_UNDERLINE);
+  attron(COLOR_PAIR(4) | A_UNDERLINE);
+  mvprintw(45, COLS / 2 - len, "*********************************");
+  attroff(COLOR_PAIR(4) | A_UNDERLINE);
 }
 
 void help()
@@ -177,14 +100,5 @@ void help()
   mvprintw(43, COLS / 2 - len + 6, "**");
   mvprintw(43, COLS / 2 - len + 9, "**");
   mvprintw(43, COLS / 2 - len + 18, "**");
-  mvprintw(43, COLS / 2 - len + 25, "**");
-  mvprintw(44, COLS / 2 - len, "**");
-  mvprintw(44, COLS / 2 - len + 6, "**");
-  mvprintw(44, COLS / 2 - len + 9, "********");
-  mvprintw(44, COLS / 2 - len + 18, "********");
-  mvprintw(44, COLS / 2 - len + 25, "**");
-  attroff(COLOR_PAIR(3));
-//   attron(COLOR_PAIR(4) | A_UNDERLINE);
-//   mvprintw(45, COLS / 2 - len, "                          ");
-//   attroff(COLOR_PAIR(4) | A_UNDERLINE);
+end_help(len);
 }
---
+++ Graphics/print_menutwo.c
@@ -0,0 +1,125 @@
+/*
+** print_menutwo.c for print_menu2 in /home/augustin/Bureau/augustin/projets_groupes/Tetris/PSU_2016_tetris/Graphics/
+**
+** Made by augustin leconte
+** Login   <augustin.leconte@epitech.eu>
+**
+** Started on  Thu Mar  2 12:08:04 2017 augustin leconte
+** Last update Thu Mar  2 12:11:39 2017 augustin leconte
+*/
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <wait.h>
+#include <curses.h>
+#include <ncurses.h>
+#include "tetris.h"
+
+int my_strlen();
+
+void print_line(int where)
+{
+  int i;
+
+  i = -1;
+  start_color();
+  init_pair(4, COLOR_WHITE, COLOR_WHITE);
+  attron(COLOR_PAIR(4));
+  while (++i < COLS)
+    mvprintw(where, i, "-");
+  attroff(COLOR_PAIR(4));
+}
+
+void end_name(int len)
+{
+  mvprintw(3, COLS / 2 - len + 42, "**");
+  mvprintw(3, COLS / 2 - len + 45, "******");
+  mvprintw(4, COLS / 2 - len + 4, "**");
+  mvprintw(4, COLS / 2 - len + 11, "**");
+  mvprintw(4, COLS / 2 - len + 24, "**");
+  mvprintw(4, COLS / 2 - len + 31, "**");
+  mvprintw(4, COLS / 2 - len + 37, "**");
+  mvprintw(4, COLS / 2 - len + 42, "**");
+  mvprintw(4, COLS / 2 - len + 49, "**");
+  mvprintw(5, COLS / 2 - len + 4, "**");
+  mvprintw(5, COLS / 2 - len + 11, "********");
+  mvprintw(5, COLS / 2 - len + 24, "**");
+  mvprintw(5, COLS / 2 - len + 31, "**");
+  mvprintw(5, COLS / 2 - len + 37, "**");
+  mvprintw(5, COLS / 2 - len + 42, "**");
+  mvprintw(5, COLS / 2 - len + 45, "******");
+  attroff(COLOR_PAIR(3));
+}
+
+void print_name()
+{
+  int len;
+
+  start_color();
+  init_pair(3, COLOR_BLUE, COLOR_BLUE);
+  attron(COLOR_PAIR(3));
+  len = 53 / 2;
+  mvprintw(1, COLS / 2 - len, "**********");
+  mvprintw(1, COLS / 2 - len + 11, "********");
+  mvprintw(1, COLS / 2 - len + 20, "**********");
+  mvprintw(1, COLS / 2 - len + 31, "******");
+  mvprintw(1, COLS / 2 - len + 42, "**");
+  mvprintw(1, COLS / 2 - len + 45, "******");
+  mvprintw(2, COLS / 2 - len + 4, "**");
+  mvprintw(2, COLS / 2 - len + 11, "**");
+  mvprintw(2, COLS / 2 - len + 24, "**");
+  mvprintw(2, COLS / 2 - len + 31, "**");
+  mvprintw(2, COLS / 2 - len + 37, "**");
+  mvprintw(2, COLS / 2 - len + 42, "**");
+  mvprintw(2, COLS / 2 - len + 45, "**");
+  mvprintw(3, COLS / 2 - len + 4, "**");
+  mvprintw(3, COLS / 2 - len + 11, "******");
+  mvprintw(3, COLS / 2 - len + 24, "**");
+  mvprintw(3, COLS / 2 - len + 31, "******");
+  end_name(len);
+}
+
+void end_play(int len)
+{
+  mvprintw(23, COLS / 2 - len, "**");
+  mvprintw(23, COLS / 2 - len + 9, "**");
+  mvprintw(23, COLS / 2 - len + 16, "**");
+  mvprintw(23, COLS / 2 - len + 22, "**");
+  mvprintw(23, COLS / 2 - len + 29, "**");
+  mvprintw(24, COLS / 2 - len, "**");
+  mvprintw(24, COLS / 2 - len + 9, "******");
+  mvprintw(24, COLS / 2 - len + 16, "**");
+  mvprintw(24, COLS / 2 - len + 22, "**");
+  mvprintw(24, COLS / 2 - len + 29, "**");
+  attroff(COLOR_PAIR(3));
+  attron(COLOR_PAIR(4) | A_UNDERLINE);
+  mvprintw(25, COLS / 2 - len, "**********************************");
+  attroff(COLOR_PAIR(4) | A_UNDERLINE);
+}
+
+void play()
+{
+  int len;
+
+  attron(COLOR_PAIR(3));
+  len = 35 / 2;
+  mvprintw(20, COLS / 2 - len, "********");
+  mvprintw(20, COLS / 2 - len + 9, "**");
+  mvprintw(20, COLS / 2 - len + 18, "****");
+  mvprintw(20, COLS / 2 - len + 25, "**");
+  mvprintw(20, COLS / 2 - len + 33, "**");
+  mvprintw(21, COLS / 2 - len, "**");
+  mvprintw(21, COLS / 2 - len + 6, "**");
+  mvprintw(21, COLS / 2 - len + 9, "**");
+  mvprintw(21, COLS / 2 - len + 16, "**");
+  mvprintw(21, COLS / 2 - len + 22, "**");
+  mvprintw(21, COLS / 2 - len + 25, "**");
+  mvprintw(21, COLS / 2 - len + 33, "**");
+  mvprintw(22, COLS / 2 - len, "********");
+  mvprintw(22, COLS / 2 - len + 9, "**");
+  mvprintw(22, COLS / 2 - len + 16, "********");
+  mvprintw(22, COLS / 2 - len + 27, "**");
+  mvprintw(22, COLS / 2 - len + 31, "**");
+  end_play(len);
+}
