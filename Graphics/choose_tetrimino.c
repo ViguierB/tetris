c52935a995e30ae1e93fb3e15f3779ef136300f6
Augustin Leconte <augustin.leconte@epitech.eu>
2017-03-10 17:46:16 +0100

    Bon ça sergfault ... go regarder tmp y a tt dedans

--- Graphics/choose_tetrimino.c
+++ Graphics/choose_tetrimino.c
@@ -5,7 +5,7 @@
 ** Login   <augustin.leconte@epitech.eu>
 **
 ** Started on  Fri Mar 10 10:50:45 2017 augustin leconte
-** Last update Fri Mar 10 16:33:18 2017 augustin leconte
+** Last update Fri Mar 10 17:17:23 2017 augustin leconte
 */
 
 #include <sys/stat.h>
@@ -24,12 +24,12 @@
 void print_pts(t_tetrimino *tetrimino)
 {
   int i;
-  i = -1;
 
-  // attron(COLOR_PAIR(3));
-  while (++i < tetrimino->h)
-    mvprintw(20, COLS - 20, "%s\n", "coucou");
-  // attroff(COLOR_PAIR(3));
+  i = -1;
+  attron(COLOR_PAIR(3));
+  while (tetrimino->pts[++i].x != -1)
+    mvprintw(20 + tetrimino->pts[i].x, COLS - 20 + tetrimino->pts[i].y, "%s\n", "*");
+  attroff(COLOR_PAIR(3));
   refresh();
 }
 
@@ -46,7 +46,6 @@ int choose_thems(t_data tetris)
   t_tetrimino *t;
 
   t = choose_tetrim(tetris);
-  i = -1;
   print_pts(t);
   choose_tetrim(tetris);
   sleep(5);
